d3281e0938fb7188c729f06fd4510bfb711ed5e5
Damian Brands <74612564+damianbrands@users.noreply.github.com>
2024-12-01 06:28:15 +0100

    star2

--- 2024/day01.py
+++ 2024/day01.py
@@ -5,16 +5,17 @@ def sol(part):
     with open("../input.txt") as file:
         lines = file.read().strip().split('\n')
         for line in lines:
-            if part == 1:
-                input_nr = line.split("   ")
-                list1.append(int(input_nr[0]))
-                list2.append(int(input_nr[1]))
-
+            input_nr = line.split("   ")
+            list1.append(int(input_nr[0]))
+            list2.append(int(input_nr[1]))
         if part == 1:
             list1.sort()
             list2.sort()
             for i, ix in enumerate(list1):
                 res += abs(list1[i - 1] - list2[i - 1])
+        if part == 2:
+            for i, ix in enumerate(list1):
+                res += ix * list2.count(ix)
     return res
 
 
