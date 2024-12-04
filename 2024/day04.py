5e8d0000af85805cfdd98c4bdc9f4c53e4281f01
Damian Brands <74612564+damianbrands@users.noreply.github.com>
2024-12-04 09:48:41 +0100

    star2

--- 2024/day04.py
+++ 2024/day04.py
@@ -5,9 +5,37 @@ def sol(part):
         grid = []
         for line in lines:
             grid.append(list(line))
-        locations = search(grid, "XMAS")
-        res += len(locations)
+        if part == 1:
+            locations = search(grid, "XMAS")
+            res += len(locations)
+        if part == 2:
+            locations = search_mas(grid, "MAS")
+            locations.sort()
+            final_loc = []
+            for location in locations:
+                if locations.count(location) > 1:
+                    final_loc.append(location)
+            res += int(len(final_loc) / 2)
         return res
+
+def search_mas(grid, word):
+    directions = {
+        "diagonal-up-left": (-1, -1),
+        "diagonal-up-right": (-1, 1),
+        "diagonal-down-left": (1, -1),
+        "diagonal-down-right": (1, 1)
+    }
+    locations = []
+    width = len(grid)
+    height = len(grid[0])
+
+    for x in range(width):
+        for y in range(height):
+            if grid[x][y] == word[0]:
+                for dirX, dirY in directions.values():
+                    if find_word(grid, width, height, word, 0, x, y, dirX, dirY):
+                        locations.append([x + dirX, y + dirY])
+    return locations
 
 
 def search(grid, word):
@@ -29,7 +57,7 @@ def search(grid, word):
         for y in range(height):
             if grid[x][y] == word[0]:
                 for dirX, dirY in directions.values():
-                    if find_word(grid, width, height, "XMAS", 0, x, y, dirX, dirY):
+                    if find_word(grid, width, height, word, 0, x, y, dirX, dirY):
                         locations.append([x, y])
     return locations
 
