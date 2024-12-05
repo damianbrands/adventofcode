7f629a8ac5b2b9cc64a1ca5302abdc3c6dc3cb55
Damian Brands <74612564+damianbrands@users.noreply.github.com>
2024-12-05 19:36:56 +0100

    star2

--- 2024/day05.py
+++ 2024/day05.py
@@ -4,29 +4,50 @@ def sol(part):
     update_orders = []
     with open("../input.txt") as file:
         lines = file.read().strip().split('\n')
-        if part == 1:
-            # Parsing
-            for line in lines:
-                if line.__contains__("|"):
-                    order_rule = []
-                    for item in line.split('|'):
-                        order_rule.append(int(item))
-                    order_rules.append(order_rule)
-                elif line.__contains__(","):
-                    update = []
-                    for item in line.split(','):
-                        update.append(int(item))
-                    update_orders.append(update)
-            # Update check
-            for update_order in update_orders:
-                correct = True
-                for update in update_order:
-                    for order_rule in order_rules:
-                        if update == order_rule[0] and update_order.__contains__(order_rule[1]):
-                            if update_order.index(update) > update_order.index(order_rule[1]):
-                                correct = False
-                if correct:
-                    res += update_order[len(update_order) // 2]
+        # Parsing
+        for line in lines:
+            if line.__contains__("|"):
+                order_rule = []
+                for item in line.split('|'):
+                    order_rule.append(int(item))
+                order_rules.append(order_rule)
+            elif line.__contains__(","):
+                update = []
+                for item in line.split(','):
+                    update.append(int(item))
+                update_orders.append(update)
+
+    wrong_orders = []
+    # Update check
+
+    for update_order in update_orders:
+        correct = True
+        for update in update_order:
+            for order_rule in order_rules:
+                if update == order_rule[0] and update_order.__contains__(order_rule[1]):
+                    if update_order.index(update) > update_order.index(order_rule[1]):
+                        correct = False
+        if correct:
+            if part == 1:
+                res += update_order[len(update_order) // 2]
+        if not correct:
+            wrong_orders.append(update_order)
+
+    if part == 2:
+        for update_order in wrong_orders:
+            while True:
+                swapped = False
+                for rule in order_rules:
+                    if rule[0] in update_order and rule[1] in update_order:
+                        index_a = update_order.index(rule[0])
+                        index_b = update_order.index(rule[1])
+                        if index_a > index_b:
+                            update_order[index_a], update_order[index_b] = update_order[index_b], update_order[index_a]
+                            swapped = True
+                            break
+                if not swapped:
+                    break
+            res += update_order[len(update_order) // 2]
     return res
 
 
